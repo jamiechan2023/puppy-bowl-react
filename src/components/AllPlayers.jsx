3a6fa5cbbbc735fe9444d39e788b4412dbb36b26
Jamie Chan <jamiechan@Jamies-MBP.lan>
2024-01-28 18:28:11 -0800

    added functionality to add new player

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PlayerCardItem from "./PlayerCardItem";
 import "./PlayerCard.css";
-//import NewPlayer from "./NewPlayer";
+import NewPlayer from "./NewPlayer";
 
-const AllPlayers = ({ players }) => {
+const AllPlayers = ({ players, setPlayers }) => {
   return (
     <div className="all-players-container">
-      {/* <NewPlayer /> */}
+      <NewPlayer setPlayers={setPlayers} />
       {players.map((player) => {
         return <PlayerCardItem key={player.id} player={player} />;
       })}
